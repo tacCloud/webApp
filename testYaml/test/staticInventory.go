4432a7ca8621b47280b37c664e3f8b46c3d310c3
McCabe, Robert J <robert.mccabe@rockwellcollins.com>
2021-01-19 20:00:42 -0600

    Added test yaml

---
+++ testYaml/test/staticInventory.go
@@ -0,0 +1,9 @@
+package webApp
+
+var fakeItems = [...]InventoryItem{
+	{ItemName: "Foosi             sssssss", Price: 37},
+	{ItemName: "Bar", Price: 42},
+	{ItemName: "Bill", Price: 42},
+	{ItemName: "Boo", Price: 42},
+	{ItemName: "Pee", Price: 42},
+}
